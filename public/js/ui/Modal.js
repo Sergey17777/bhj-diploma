5acfd29b8dbeef4278dea52a91cf3c5c5b2eafb2
svgonline@ya.ru <svgonline@ya.ru>
2022-05-01 19:02:14 +0500

    sd

---
+++ public/js/ui/Modal.js
@@ -0,0 +1,59 @@
+/**
+ * Класс Modal отвечает за
+ * управление всплывающими окнами.
+ * В первую очередь это открытие или
+ * закрытие имеющихся окон
+ * */
+class Modal {
+  /**
+   * Устанавливает текущий элемент в свойство element
+   * Регистрирует обработчики событий с помощью Modal.registerEvents()
+   * Если переданный элемент не существует,
+   * необходимо выкинуть ошибку.
+   * */
+  constructor(element){
+    if (!element) {
+      throw new Error("Невалидное значение для Modal");
+    }
+    this.element = element;
+    this.registerEvents();
+  }
+
+  /**
+   * При нажатии на элемент с data-dismiss="modal"
+   * должен закрыть текущее окно
+   * (с помощью метода Modal.onClose)
+   * */
+  registerEvents() {
+    const elementBtns = this.element.getElementsByTagName('button');
+    for (let button of elementBtns) {
+      if (button.hasAttribute('data-dismiss') && button.dataset.dismiss === 'modal') {
+        button.addEventListener('click', (ev) => {
+          ev.preventDefault();
+          this.onClose(button);
+        });
+      }
+    }
+  }
+
+  /**
+   * Срабатывает после нажатия на элементы, закрывающие окно.
+   * Закрывает текущее окно (Modal.close())
+   * */
+  onClose(e) {
+    e.onClick = this.close();
+  }
+  /**
+   * Открывает окно: устанавливает CSS-свойство display
+   * со значением «block»
+   * */
+  open() {
+    this.element.style.display = 'block';
+  }
+  /**
+   * Закрывает окно: удаляет CSS-свойство display
+   * */
+  close(){
+    this.element.removeAttribute('style');
+  }
+}
---
+++ public/js/ui/widgets/TransactionsWidget.js
@@ -0,0 +1,43 @@
+/**
+ * Класс TransactionsWidget отвечает за
+ * открытие всплывающих окон для
+ * создания нового дохода или расхода
+ * */
+
+class TransactionsWidget {
+  /**
+   * Устанавливает полученный элемент
+   * в свойство element.
+   * Если переданный элемент не существует,
+   * необходимо выкинуть ошибку.
+   * */
+  constructor( element ) {
+    if (!element) {
+      throw new Error("Невалидное значение для TransactionsWidget"); 
+    }
+    this.element = element;
+    this.registerEvents();
+  }
+  /**
+   * Регистрирует обработчики нажатия на
+   * кнопки «Новый доход» и «Новый расход».
+   * При нажатии вызывает Modal.open() для
+   * экземпляра окна
+   * */
+  registerEvents() {
+    const transactionBtns = document.querySelectorAll('.transactions-panel > button');
+    transactionBtns.forEach(button => {
+      button.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        switch (ev.target.closest('button').classList[3]) {
+          case 'create-income-button':
+            App.getModal('newIncome').open();
+          break;
+          case 'create-expense-button':
+            App.getModal('newExpense').open();
+          break;
+        }
+      });
+    });
+  }
+}
